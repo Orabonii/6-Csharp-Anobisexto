86f3e4c81875d54c178b0539cdb769c910d50f7b
kauã oraboni <70423924+Orabonii@users.noreply.github.com>
2024-06-08 20:52:34 -0300

    Add files via upload

---
+++ exercicio06/main.cs
@@ -0,0 +1,31 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite um ano: ");
+        int ano = int.Parse(Console.ReadLine());
+
+        if (EhBissexto(ano))
+        {
+            Console.WriteLine($"{ano} é um ano bissexto.");
+        }
+        else
+        {
+            Console.WriteLine($"{ano} não é um ano bissexto.");
+        }
+    }
+
+    static bool EhBissexto(int ano)
+    {
+        if ((ano % 4 == 0 && ano % 100 != 0) || ano % 400 == 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
